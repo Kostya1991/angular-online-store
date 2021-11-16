fa17670a3821bfe5ff31a55b942e208f987c2cea
Lysov.Konstantin <Kosta-Lysov@mail.ru>
2021-11-16 14:30:38 +0300

    добавлен конфиг firebase

---
+++ firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC7kNyoHT_XBCUSWozQ6htPmW_6ZJPV4oo",
+  authDomain: "online-store-a1bed.firebaseapp.com",
+  databaseURL: "https://online-store-a1bed-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "online-store-a1bed",
+  storageBucket: "online-store-a1bed.appspot.com",
+  messagingSenderId: "178332770189",
+  appId: "1:178332770189:web:1ab63d6a8e5c753659a7c4"
+};
